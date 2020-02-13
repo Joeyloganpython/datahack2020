01b4564e98c779536a31e0a7ce1e35f33f69f1a0
Maltenfort, Mitchell <MALTENFORM@EMAIL.CHOP.EDU>
2020-02-13 11:23:07 -0500

    Updated for readability and county effects

--- OD_modeling MGM.R
+++ OD_modeling MGM.R
@@ -30,24 +30,31 @@ levels(OD_all$Race2) <- c("White", "Other or Unknown", "Other or Unknown", "Blac
 OD_all$Ethnicity.Desc <- relevel(as.factor(OD_all$Ethnicity.Desc), "Not Hispanic")
 OD_all$HEROIN <- as.factor(OD_all$HEROIN)
 OD_all$FENTANYL <- as.factor(OD_all$FENTANYL)
+OD_all$UNKNOWN <- as.factor(OD_all$UNKNOWN)
+
+OD_all$Female <- OD_all$Gender.Desc == "Female"
 
 library(rms)
-frm_glm <- Died ~ Naloxone.Administered * (HEROIN * FENTANYL + Age2) + Race2 + UNKNOWN
-OD_lrm <- lrm(frm_glm, OD_all, x = TRUE, y = TRUE)
+frm_died <- Died ~ Naloxone.Administered * (HEROIN * FENTANYL + UNKNOWN + Age2) + Race2  + Female
+frm_lived <- Lived ~ Naloxone.Administered * (HEROIN * FENTANYL + UNKNOWN + Age2) + Race2  + Female
 
-glm1 <- glm(frm_glm, data = OD_all, family = binomial)
-glm2 <- glmer(update(frm_glm, . ~ . + (1 | Incident.County.FIPS.Code)), data = OD_all, family = binomial)
-glm3 <- glmer(update(frm_glm, . ~ . + (Naloxone.Administered | Incident.County.FIPS.Code)), data = OD_all, family = binomial)
+lrm_died <- lrm(frm_died, OD_all, x = TRUE, y = TRUE)
+lrm_lived <- lrm(frm_lived, OD_all, x = TRUE, y = TRUE)
 
-anova(glm3, glm2, glm1)
+val_died <- rms::validate(lrm_died, B = 200)
+val_lived <- rms::validate(lrm_lived, B = 200)
 
-library(ggeffects)
-library(sjPlot)
-ggeffect(glm3, terms = c("Naloxone.Administered", "HEROIN", "FENTANYL"))
+cal_died <- calibrate(lrm_died, B = 200)
+cal_lived <- calibrate(lrm_lived, B = 200)
 
-glm4 <- glmer(update(frm_glm, Lived ~ . + (Naloxone.Administered | Incident.County.FIPS.Code)), data = OD_all, family = binomial)
+glm_died_1 <- glm(frm_died, data = OD_all, family = binomial)
+glm_died_2 <- glmer(update(frm_died, . ~ . + (1 | Incident.County.FIPS.Code)), data = OD_all, family = binomial)
+glm_died_3 <- glmer(update(frm_died, . ~ . + (Naloxone.Administered | Incident.County.FIPS.Code)), data = OD_all, family = binomial)
 
-ggeffect(glm4, terms = c("Naloxone.Administered", "HEROIN", "FENTANYL"))
+
+anova(glm_died_3, glm_died_2, glm_died_1)
+
+drop1(glm_died_3, test = "Chisq")
 
 
 OD_all %>%
@@ -56,19 +63,105 @@ OD_all %>%
   arrange(Naloxone.Administered, HEROIN, FENTANYL)
 
 OD_all %>%
-  group_by(HEROIN, FENTANYL, Naloxone.Administered) %>%
-  summarise(n = n(), Lived = sum(Lived), rate = mean(Lived)) %>%
-  arrange(Naloxone.Administered, HEROIN, FENTANYL)
+  group_by(UNKNOWN, Naloxone.Administered) %>%
+  summarise(n = n(), Died = sum(Died), rate = mean(Died)) %>%
+  arrange(Naloxone.Administered, UNKNOWN)
+
+
+
+library(ggeffects)
+library(sjPlot)
+library(scales)
+
+
+
+
 
 OD_all %>%
-  group_by(Naloxone.Administered,Age2) %>%
-  summarise(n = n(), Died = sum(Died),Lived=sum(Lived)) %>%
-  arrange(Naloxone.Administered,Age2)
+  group_by(Naloxone.Administered, Age2) %>%
+  summarise(n = n(), Died = sum(Died), Lived = sum(Lived)) %>%
+  arrange(Naloxone.Administered, Age2)
 
-ggeffect(glm3, terms = c("Naloxone.Administered", "Age2"))
+library(tableone)
 
-plot_model(glm3)
-plot_model(glm4)
+table_vars <- c("Age.Range", "Race", "Ethnicity.Desc", "Gender.Desc", "HEROIN", "FENTANYL", "UNKNOWN", "Survive")
+factor_vars <- table_vars
+table1 <- CreateTableOne(table_vars, "Naloxone.Administered", OD_all, factor_vars)
 
-plot_model(glm3,"re",axis.lim = c(0.1,10))
-plot_model(glm4,"re",axis.lim = c(0.1,10))
+table_vars <- c("Age.Range", "Race", "Ethnicity.Desc", "Gender.Desc", "Naloxone.Administered", "UNKNOWN", "Survive")
+factor_vars <- table_vars
+table2 <- CreateTableOne(table_vars, c("HEROIN", "FENTANYL"), OD_all, factor_vars)
+
+table_vars <- c("Age.Range","Race", "Ethnicity.Desc", "Gender.Desc", "HEROIN", "FENTANYL", "Naloxone.Administered", "Survive")
+factor_vars <- table_vars
+table3 <- CreateTableOne(table_vars, "UNKNOWN", OD_all, factor_vars)
+
+#propensity score matching was the intent, but also useful to identify patterns in useage of Naxalone
+
+OD_all$treat <- as.numeric(OD_all$Naloxone.Administered=="Y")
+
+frm_ps <- treat ~ HEROIN * FENTANYL + UNKNOWN +  Ethnicity.Desc +
+  Age2 + Race2 + Female + (1|Incident.County.FIPS.Code)
+
+glm_ps <-glmer(frm_ps,family=binomial,data=OD_all)
+drop1(glm_ps,test="Chisq")
+
+library(survey)
+
+ps=fitted(glm_ps)
+
+OD_all$psw <- ifelse(OD_all$Naloxone.Administered=="Y",1/ps,1/(1-ps))
+
+design_ps <-svydesign(ids=~Incident.County.FIPS.Code,weights=~psw,data=OD_all)
+
+ps_model <-svyglm(Died~Naloxone.Administered*(HEROIN * FENTANYL+UNKNOWN+Age2),family=quasibinomial,design=design_ps)
+
+table_vars <- c("Age.Range", "Race", "Ethnicity.Desc", "Gender.Desc", "HEROIN", "FENTANYL", "UNKNOWN", "Survive")
+factor_vars <- table_vars
+svyCreateTableOne(table_vars,"Naloxone.Administered" ,design_ps,factor_vars)
+
+
+### county and age effects
+
+
+age_effect_raw <- OD_all %>%
+  mutate(Age2 = case_when(Age2 == "15 - 19" ~ "19 or less", Age2 == "60 - 69" ~ "60 or more", TRUE ~ Age2)) %>%
+  group_by(Age2, Naloxone.Administered) %>%
+  summarise(N = n(), r_died = mean(Died), r_lived = mean(Lived)) %>%
+  mutate(se_died = sqrt(r_died * (1 - r_died) / N), se_lived = sqrt(r_lived * (1 - r_lived) / N))
+
+age_effect_raw %>%
+  mutate(lower = r_died - 1.96 * se_died, upper = r_died + 1.96 * se_died) %>%
+  ggplot(aes(x = Age2, y = r_died, color = Naloxone.Administered)) + geom_point(size = 3) + geom_errorbar(aes(ymin = lower, ymax = upper), width = 0, size = 1) + theme_classic() + scale_y_continuous(label = percent) + xlab("Age Bracket") + ylab("% Died") + theme(axis.text = element_text(size = rel(1)))
+
+age_effect_raw %>%
+  mutate(lower = r_lived - 1.96 * se_lived, upper = r_lived + 1.96 * se_lived) %>%
+  ggplot(aes(x = Age2, y = r_lived, color = Naloxone.Administered)) + geom_point(size = 3) + geom_errorbar(aes(ymin = lower, ymax = upper), width = 0, size = 1) + theme_classic() + scale_y_continuous(label = percent) + xlab("Age Bracket") + ylab("% Lived") + theme(axis.text = element_text(size = rel(1)))
+
+
+county_effect_raw <- OD_all %>%
+  group_by(Naloxone.Administered, Incident.County.FIPS.Code) %>%
+  summarise(N = n(), r_died = mean(Died), r_lived = mean(Lived)) %>%
+  mutate(se_died = sqrt(r_died * (1 - r_died) / N), se_lived = sqrt(r_lived * (1 - r_lived) / N))
+
+county_effect_raw %>%
+  ggplot(aes(x = r_died)) + geom_histogram() + facet_wrap(Naloxone.Administered ~ ., ncol = 1, labeller = label_both) + theme_classic() + xlab("% died") + ylab("# of counties") + scale_x_continuous(label = percent) + theme(axis.text = element_text(size = rel(1.0)), strip.text = element_text(size = rel(1.0)))
+
+county_effect_raw %>%
+  pivot_wider(id_cols = Incident.County.FIPS.Code, names_from = Naloxone.Administered, values_from = r_died) %>%
+  ggplot(aes(x = N, y = Y)) + geom_point() + stat_smooth() + theme_classic() + xlab("% died, no Narcan") + ylab("% died, with Narcan") + scale_x_continuous(label = percent) + scale_y_continuous(label = percent) + theme(axis.text = element_text(size = rel(1.0))) + coord_cartesian(xlim = c(0, 1), ylim = c(0, 1))
+
+county_narcan <- OD_all %>%
+  group_by(Incident.County.FIPS.Code) %>%
+  summarise(N = n(), r_treat = mean(treat)) %>%
+  mutate(se_treat = sqrt(r_treat * (1 - r_treat) / N))
+              
+
+county_narcan %>%
+  ggplot(aes(x = r_treat)) + geom_histogram()  + theme_classic() + xlab("% cases treated") + ylab("# of counties") + scale_x_continuous(label = percent) + theme(axis.text = element_text(size = rel(1.0)), strip.text = element_text(size = rel(1.0)))
+
+
+county_narcan %>%
+  ggplot(aes(x = N,y=r_treat)) + geom_point() +geom_smooth()  + theme_classic() + xlab("# of cases per county") + ylab("% treated") + scale_y_continuous(label = percent) + theme(axis.text = element_text(size = rel(1.0)), strip.text = element_text(size = rel(1.0)))+scale_x_log10()
+    
+  )
